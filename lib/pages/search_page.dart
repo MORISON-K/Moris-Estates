396c5a1b93bf238487167e8e25c638a0b3d8dd82
Morison <morisonabinsinguza@gmail.com>
2025-06-16 16:16:36 +0300

    Update favorite icon

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -34,101 +34,123 @@ class SearchPage extends StatelessWidget {
         padding: const EdgeInsets.all(12),
         itemCount: items.length,
         itemBuilder: (context, index) {
-          final item = items[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            elevation: 6,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Image Section
-                ClipRRect(
-                  borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
-                  child: Image.asset(
-                    item['image']!,
-                    width: double.infinity,
-                    height: 180,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-
-                // Content Section
-                Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        item['category']!,
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.teal[700],
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    
-                        Row(
-                        children: [
-
-                          const SizedBox(width: 4),
-                          Expanded(
-                            child: Text(
-                             'UGX ${item['price']}', 
-                               style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black87,
-                         ),
-                            ),
-                            
-                          ),
-                          Icon(Icons.favorite)
-                        ],
-                      ), 
-
-                      const SizedBox(height: 5),
-                      
-                      Text(
-                        item['description']!,
-                        style: const TextStyle(fontSize: 15, color: Colors.black54),
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        children: [
-                          const Icon(Icons.location_on, size: 18, color: Colors.grey),
-                          const SizedBox(width: 4),
-                          Expanded(
-                            child: Text(
-                              item['location']!,
-                              style: const TextStyle(color: Colors.black87),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 6),
-                      Row(
-                        children: [
-                          const Icon(Icons.person, size: 18, color: Colors.grey),
-                          const SizedBox(width: 4),
-                          Expanded(
-                            child: Text(
-                              item['seller']!,
-                              style: const TextStyle(color: Colors.black87),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          );
+          return PropertyCard(item: items[index]);
         },
       ),
     );
   }
 }
+
+
+class PropertyCard extends StatefulWidget {
+  final Map<String, String> item;
+
+  const PropertyCard({super.key, required this.item});
+
+  @override
+  State<PropertyCard> createState() => _PropertyCardState();
+}
+
+class _PropertyCardState extends State<PropertyCard> {
+   bool isFavorite = false; 
+
+  @override
+  Widget build(BuildContext context) {
+    final item  = widget.item;
+    return Card(
+       margin: const EdgeInsets.symmetric(vertical: 10),
+      elevation: 6,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+           ClipRRect(
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
+            child: Image.asset(
+              item['image']!,
+              width: double.infinity,
+              height: 180,
+              fit: BoxFit.cover,
+            ),
+          ),
+           
+          Padding(
+             padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  item['category']!,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.teal[700],
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const SizedBox(width: 4),
+                    Expanded(
+                      child: Text(
+                        'UGX ${item['price']}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color:  isFavorite ? Colors.red : Colors.grey,
+                    ),
+                    onPressed: (){
+                      setState(() {
+                        isFavorite = !isFavorite;
+                      });
+                    },
+               ),
+           ],
+      ),
+       const SizedBox(height: 5),
+         Text(
+                  item['description']!,
+                  style: const TextStyle(fontSize: 15, color: Colors.black54),
+                ),
+      const SizedBox(height: 8),
+       Row(
+                  children: [
+                    const Icon(Icons.location_on, size: 18, color: Colors.grey),
+                    const SizedBox(width: 4),
+                    Expanded(
+                      child: Text(
+                        item['location']!,
+                        style: const TextStyle(color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                ),
+        const SizedBox(height: 6),
+                Row(
+                  children: [
+                    const Icon(Icons.person, size: 18, color: Colors.grey),
+                    const SizedBox(width: 4),
+                    Expanded(
+                      child: Text(
+                        item['seller']!,
+                        style: const TextStyle(color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+} 
